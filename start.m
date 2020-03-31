2a70ed3f2fc69d3bbebe442750ceac4065ea2144
zyw271828 <zyw_0618@outlook.com>
2020-03-31 21:16:50 +0800

    Implement hamming ranking

--- start.m
+++ start.m
@@ -6,7 +6,7 @@ datasetCandi = {'siftsmall'};
 methodCandi = {'LSH'};
 % methodCandi = {'AGH1', 'AGH2', 'BRE', 'CH', 'CPH', 'DSH', 'IsoH', 'ITQ', 'KLSH', 'LSH', 'SH', 'SpH', 'USPLH'};
 
-codelengthCandi = [32];
+codelengthCandi = [1024];
 % codelengthCandi = [32, 64, 128, 256, 512, 1024];
 
 for d = 1:length(datasetCandi)
@@ -16,15 +16,14 @@ for d = 1:length(datasetCandi)
         method = methodCandi{m};
 
         for c = 1:length(codelengthCandi)
-
             codelength = codelengthCandi(c);
 
-            ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'table', upper(dataset), '32b_1'];
-            bFound = checkFILEmkDIR(ResultFile);
-
-            if (bFound)
-                error('Result file exists!');
-            end
+            % ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'table', upper(dataset), '32b_1'];
+            % bFound = checkFILEmkDIR(ResultFile);
+            %
+            % if (bFound)
+            %     error('Result file exists!');
+            % end
 
             disp('==============================');
             disp([method, ' ', num2str(codelength), 'bit ', dataset]);
@@ -35,8 +34,8 @@ for d = 1:length(datasetCandi)
             trainset = trainset';
             testset = testset';
 
-            trainStr = ['[model, trainB ,train_elapse] = ', method, '_learn(trainset, codelength);'];
-            testStr = ['[testB,test_elapse] = ', method, '_compress(testset, model);'];
+            trainStr = ['[model, trainB, train_elapse] = ', method, '_learn(trainset, codelength);'];
+            testStr = ['[testB, test_elapse] = ', method, '_compress(testset, model);'];
             eval(trainStr);
             eval(testStr);
 
@@ -44,101 +43,38 @@ for d = 1:length(datasetCandi)
             ntest = size(testB, 1);
 
             realcodelength = size(trainB, 2);
-            disp([method, ' ', num2str(realcodelength), 'bit learned. ', dataset]);
-
-            nFile = fix(realcodelength / 32);
-            nRemain = mod(realcodelength, 32);
-
+            disp([method, ' ', num2str(realcodelength), 'bit learned.', dataset]);
             disp('==============================');
             disp(['Total training time: ', num2str(train_elapse)]);
             disp(['Total testing time: ', num2str(test_elapse)]);
 
-            %continue;
+            % start searching
+            groundtruthset = ivecs_read(['./dataset/', dataset, '/', dataset, '_groundtruth.ivecs']);
+            groundtruthset = groundtruthset';
+            res = Inf(length(groundtruthset) + 1, 2);
 
-            for j = 1:nFile
-                ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'table', upper(dataset), '32b_', num2str(j)];
+            tmp_T = tic;
 
-                fid = fopen(ResultFile, 'w');
-                fwrite(fid, 1, 'uint32');
-                fwrite(fid, 32, 'uint32');
-                fwrite(fid, ntrain, 'uint32');
+            for ii = 1:size(testB, 1)
 
-                for i = 1:ntrain
-                    tmpV = uint32(0);
-
-                    for k = 32 * (j - 1) + 1:32 * j
-                        tmpV = bitshift(tmpV, 1);
-                        tmpV = tmpV + uint32(trainB(i, k));
-                    end
-
-                    fwrite(fid, tmpV, 'uint32');
+                for jj = 1:size(trainB, 1)
+                    res(length(groundtruthset) + 1, 1) = jj;
+                    res(length(groundtruthset) + 1, 2) = hammingDist(testB(ii, :), trainB(jj, :));
+                    res = sortrows(res, 2); % sort by hamming distance
                 end
 
-                fclose(fid);
-
-                ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'query', upper(dataset), '32b_', num2str(j)];
-                fid = fopen(ResultFile, 'w');
-                fwrite(fid, 1, 'uint32');
-                fwrite(fid, 32, 'uint32');
-                fwrite(fid, ntest, 'uint32');
-
-                for i = 1:ntest
-                    tmpV = uint32(0);
-
-                    for k = 32 * (j - 1) + 1:32 * j
-                        tmpV = bitshift(tmpV, 1);
-                        tmpV = tmpV + uint32(testB(i, k));
-                    end
-
-                    fwrite(fid, tmpV, 'uint32');
-                end
-
-                fclose(fid);
+                res(end, :) = [];
+                disp(['Query number: ', num2str(ii)])
+                disp(res);
             end
 
-            if nRemain > 0
-                ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'table', upper(dataset), '32b_', num2str(nFile + 1)];
+            search_elapse = toc(tmp_T);
+            disp(['Search time (', num2str(codelength), '-bits ', method, '): ', num2str(search_elapse)]);
 
-                fid = fopen(ResultFile, 'w');
-                fwrite(fid, 1, 'uint32');
-                fwrite(fid, 32, 'uint32');
-                fwrite(fid, ntrain, 'uint32');
-
-                for i = 1:ntrain
-                    tmpV = uint32(0);
-
-                    for k = 32 * nFile + 1:size(trainB, 2)
-                        tmpV = bitshift(tmpV, 1);
-                        tmpV = tmpV + uint32(trainB(i, k));
-                    end
-
-                    fwrite(fid, tmpV, 'uint32');
-                end
-
-                fclose(fid);
-
-                ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'query', upper(dataset), '32b_', num2str(nFile + 1)];
-                fid = fopen(ResultFile, 'w');
-                fwrite(fid, 1, 'uint32');
-                fwrite(fid, 32, 'uint32');
-                fwrite(fid, ntest, 'uint32');
-
-                for i = 1:ntest
-                    tmpV = uint32(0);
-
-                    for k = 32 * nFile + 1:size(testB, 2)
-                        tmpV = bitshift(tmpV, 1);
-                        tmpV = tmpV + uint32(testB(i, k));
-                    end
-
-                    fwrite(fid, tmpV, 'uint32');
-                end
-
-                fclose(fid);
-
-            end
-
-            disp('binary file saved!');
+            % Evaluation
+            % ivecs_read('./dataset/siftsmall/siftsmall_groundtruth.ivecs')'
+            % first query: ans(1, :)' + 1
+            % calc
         end
 
     end
