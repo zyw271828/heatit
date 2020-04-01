cea84100e8ea0269474c3bafba45276e28055163
zyw271828 <zyw_0618@outlook.com>
2020-04-01 17:58:37 +0800

    Optimize calculation method

--- start.m
+++ start.m
@@ -3,10 +3,10 @@ addpath(genpath('./'))
 datasetCandi = {'siftsmall'};
 % datasetCandi = {'siftsmall', 'sift', 'gist'};
 
-methodCandi = {'LSH'};
+methodCandi = {'LSH', 'SpH'};
 % methodCandi = {'AGH1', 'AGH2', 'BRE', 'CH', 'CPH', 'DSH', 'IsoH', 'ITQ', 'KLSH', 'LSH', 'SH', 'SpH', 'USPLH'};
 
-codelengthCandi = [1024];
+codelengthCandi = [32, 64];
 % codelengthCandi = [32, 64, 128, 256, 512, 1024];
 
 for d = 1:length(datasetCandi)
@@ -18,22 +18,16 @@ for d = 1:length(datasetCandi)
         for c = 1:length(codelengthCandi)
             codelength = codelengthCandi(c);
 
-            % ResultFile = ['./result/', dataset, '/hashingCodeLong.', num2str(codelength), '/', method, 'table', upper(dataset), '32b_1'];
-            % bFound = checkFILEmkDIR(ResultFile);
-            %
-            % if (bFound)
-            %     error('Result file exists!');
-            % end
-
             disp('==============================');
-            disp([method, ' ', num2str(codelength), 'bit ', dataset]);
-            disp('==============================');
+            disp([num2str(codelength), '-bits ', method, ' on ', dataset]);
+            disp('------------------------------');
 
             trainset = double(fvecs_read(['./dataset/', dataset, '/', dataset, '_base.fvecs']));
             testset = fvecs_read(['./dataset/', dataset, '/', dataset, '_query.fvecs']);
             trainset = trainset';
             testset = testset';
 
+            % start training and testing
             trainStr = ['[model, trainB, train_elapse] = ', method, '_learn(trainset, codelength);'];
             testStr = ['[testB, test_elapse] = ', method, '_compress(testset, model);'];
             eval(trainStr);
@@ -43,20 +37,19 @@ for d = 1:length(datasetCandi)
             ntest = size(testB, 1);
 
             realcodelength = size(trainB, 2);
-            disp([method, ' ', num2str(realcodelength), 'bit learned.', dataset]);
-            disp('==============================');
             disp(['Total training time: ', num2str(train_elapse)]);
             disp(['Total test time: ', num2str(test_elapse)]);
 
             % start searching
             groundtruthset = ivecs_read(['./dataset/', dataset, '/', dataset, '_groundtruth.ivecs']);
             groundtruthset = groundtruthset';
-
-            tmp_T = tic;
+            recall_sum = 0; % used to calculate average recall
+            search_time_sum = 0;
 
             for ii = 1:size(testB, 1)
 
                 res = Inf(length(groundtruthset) + 1, 2);
+                tmp_T = tic;
 
                 for jj = 1:size(trainB, 1)
                     res(length(groundtruthset) + 1, 1) = jj;
@@ -65,17 +58,20 @@ for d = 1:length(datasetCandi)
                 end
 
                 res(end, :) = [];
-                disp(['Query number: ', num2str(ii)])
+
+                search_time = toc(tmp_T);
+                search_time_sum = search_time_sum + search_time;
+
+                % disp(['Query number: ', num2str(ii)])
                 % groundtruthset data is numbered from 0
                 recall = length(intersect(res(:, 1), groundtruthset(ii, :)' + 1)) / length(groundtruthset);
-                disp(['recall: ', num2str(recall)])
+                recall_sum = recall_sum + recall;
+                % disp(['recall: ', num2str(recall)])
                 % disp(res);
-                disp('==============================');
             end
 
-            search_elapse = toc(tmp_T);
-            disp(['Total search time (', num2str(codelength), '-bits ', method, '): ', num2str(search_elapse)]);
-
+            disp(['Total search time: ', num2str(search_time_sum)]);
+            disp(['Average recall: ', num2str(recall_sum / length(groundtruthset))]);
         end
 
     end
